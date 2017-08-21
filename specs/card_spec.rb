b4951dced49cafe5684d47c6f487736b5356c790
Shaunna <skwiens@gmail.com>
2017-08-21 08:33:52 -0700

    Card spec finished

--- specs/card_spec.rb
+++ specs/card_spec.rb
@@ -23,11 +23,27 @@ describe Card do
   describe "Testing to_s" do
 
     it "to_s returns a readable String value logically for values 2-10" do
-      # Test to ensure that to_s works for cards values 2-10
-      # for example:  "2 of diamonds"
+      card1 = Card.new(2, :hearts)
+      card2 = Card.new(10, :clubs)
+      card3 = Card.new(3, :spades)
+      card4 = Card.new(4, :diamonds)
+
+      card1.to_s.must_equal "2 of hearts"
+      card2.to_s.must_equal "10 of clubs"
+      card3.to_s.must_equal "3 of spades"
+      card4.to_s.must_equal "4 of diamonds"
     end
 
     it "to_s returns a readable String value for Ace, Jack, Queen, King" do
+      card1 = Card.new(11, :hearts)
+      card2 = Card.new(1, :clubs)
+      card3 = Card.new(13, :spades)
+      card4 = Card.new(12, :diamonds)
+
+      card1.to_s.must_equal "Jack of hearts"
+      card2.to_s.must_equal "Ace of clubs"
+      card3.to_s.must_equal "King of spades"
+      card4.to_s.must_equal "Queen of diamonds"
       # Test to ensure that to_s works for cards values 1, and 11-13
       # For example: "Queen of hearts"
     end
@@ -36,12 +52,32 @@ describe Card do
   describe "Reader methods" do
 
     it "Can retrieve the value of the card using a `.value`." do
+      card1 = Card.new(1, :clubs)
+      card2 = Card.new(13, :spades)
+      card3 = Card.new(2, :hearts)
+      card4 = Card.new(10, :clubs)
+
+
+      card1.value.must_equal 1
+      card2.value.must_equal 13
+      card3.value.must_equal 2
+      card4.value.must_equal 10
+
       # ensure that `.value works as expected`
+
     end
 
     it "Can retrieve the value of the card using a `.suit`." do
       # ensure that `.suit works as expected returning the symbol of the suit`
+      card1 = Card.new(1, :clubs)
+      card2 = Card.new(13, :spades)
+      card3 = Card.new(2, :hearts)
+      card4 = Card.new(10, :clubs)
 
+      card1.suit.must_equal :clubs
+      card2.suit.must_equal :spades
+      card3.suit.must_equal :hearts
+      card4.suit.must_equal :clubs
     end
   end
 
--- specs/deck_spec.rb
+++ specs/deck_spec.rb
@@ -3,9 +3,29 @@
 require_relative 'spec_helper'
 
 describe Deck do
+  before do
+    values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    suits = [:hearts, :clubs, :diamonds, :spades]
+
+    @deck1 = Deck.new([values], [suits])
+  end
+
+  describe "You can create a deck instance" do
+    it "Can be created" do
+
+      @deck1.must_be_instance_of Deck
+    end
+
+    it "Contains 52 Card objects that are unique" do
+
+      @deck1.cards.length.must_equal 52
+
+    end
+  end
+end
+
+
   # Write tests for the Deck class.
   # What should you test?
     # 1.  Expected Behavior
     # 2.  Edge cases
-
-end
