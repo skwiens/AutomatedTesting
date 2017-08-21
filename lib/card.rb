909d3ae783c0a181777447738dad2ea63d199089
Shaunna <skwiens@gmail.com>
2017-08-21 08:33:12 -0700

    Card testing and program finished.

--- lib/card.rb
+++ lib/card.rb
@@ -5,14 +5,52 @@ class Card
   attr_reader :value, :suit
 
   def initialize(value, suit)
+    if value > 13
+      raise ArgumentError.new "Value cannot be higher than 13"
+    end
+
+    if value < 1
+      raise ArgumentError.new "Value cannot be lower than 1"
+    end
+
+    unless [:hearts, :spades, :clubs, :diamonds].include?(suit)
+      raise ArgumentError.new "Not a correct suit"
+    end
+
     @value = value
     @suit = suit
-    
-
   end
 
   def to_s
-    return "#{value} of #{suit.to_s}"
+    if value < 11 && value > 1
+      return "#{value} of #{suit.to_s}"
+    elsif value == 1 || (value > 10 && value < 14)
+      case value
+      when 1
+        face = "Ace"
+      when 11
+        face = "Jack"
+      when 12
+        face = "Queen"
+      when 13
+        face = "King"
+      else
+        raise ArgumentError.new "Not a valid suit"
+      end
+
+      "#{face} of #{suit.to_s}"
+    end
+    #write a statement that returns face card values.
   end
 
 end
+
+
+# card1 = Card.new(1, :clubs)
+# card2 = Card.new(3, :hearts)
+#
+# puts card1.value
+# puts card1.suit
+#
+# puts card2.value
+# puts card2.suit
--- lib/deck.rb
+++ lib/deck.rb
@@ -5,15 +5,26 @@ require_relative 'card'
 
 class Deck
 
-  def initialize
-
+  def initialize (values, suits)
+    @cards = []
+    suits.each do |suit|
+      values.each do |value|
+        card = Card.new(value,suit)
+        
+      end
+    end
   end
 
   def draw
-    # returns a card
+
   end
 
   def shuffle
     # shuffles the deck
   end
 end
+
+values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+suits = [:hearts, :clubs, :diamonds, :spades]
+
+deck1 = Deck.new(values, suits)
